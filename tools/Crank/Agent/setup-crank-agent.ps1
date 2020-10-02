e566ca06d84366645fadd783dd99ba3c2796574d
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-01 17:38:08 -0700

    Fix Docker path

--- tools/Crank/Agent/setup-crank-agent.ps1
+++ tools/Crank/Agent/setup-crank-agent.ps1
@@ -109,7 +109,7 @@ function InstallCrankAgent {
             ./build.sh
 
             # Build the functions-docker-agent image
-            Set-Location $PSScriptRoot/../Docker
+            Set-Location $PSScriptRoot/../Linux/Docker
             ./build.sh
             ./run.sh
         } finally {
