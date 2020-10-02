0d73279e820aa967c849dc47fccb458a98bac062
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-01 22:04:51 -0700

    Remove temporary tracing

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo --- Cloning azure-functions-host repo
 mkdir /home/Functions/github
 cd /home/Functions/github
 git clone https://github.com/Azure/azure-functions-host.git
 cd azure-functions-host
 git checkout anatolib/crank-linux-container
 
-echo --- Fixing script files format and permissions
 cd tools/Crank/Agent
 sudo find . -name "*.sh" -exec sudo chmod +xr {} \;
 sudo find . -name "*.ps1" -exec sudo chmod +xr {} \;
@@ -19,5 +17,4 @@ sudo find . -name "*.ps1" -exec dos2unix {} \;
 
 Linux/install-powershell.sh
 
-echo --- Invoking setup-crank-agent-json.ps1 with parameters: $1
 ./setup-crank-agent-json.ps1 -ParametersJson $1 -Verbose
