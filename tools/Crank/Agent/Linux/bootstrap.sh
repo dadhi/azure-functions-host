dd1a172c4c5c09c94cbffe0b85c397bc2cd5e45a
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-01 22:04:11 -0700

    Remove dos2unix

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -10,11 +10,6 @@ cd tools/Crank/Agent
 sudo find . -name "*.sh" -exec sudo chmod +xr {} \;
 sudo find . -name "*.ps1" -exec sudo chmod +xr {} \;
 
-sudo apt-get update
-sudo apt-get install -y dos2unix
-sudo find . -name "*.sh" -exec dos2unix {} \;
-sudo find . -name "*.ps1" -exec dos2unix {} \;
-
 Linux/install-powershell.sh
 
 ./setup-crank-agent-json.ps1 -ParametersJson $1 -Verbose
