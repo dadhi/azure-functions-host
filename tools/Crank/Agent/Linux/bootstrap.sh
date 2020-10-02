c92c15d7aa0241fd697cb0ca087872636962c002
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-01 22:06:03 -0700

    Use ~

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /home/Functions/github
-cd /home/Functions/github
+mkdir ~/github
+cd ~/github
 git clone https://github.com/Azure/azure-functions-host.git
 cd azure-functions-host
 git checkout anatolib/crank-linux-container
